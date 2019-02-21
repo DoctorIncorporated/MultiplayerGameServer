2266ec7c43920f11b467b8e06c7f8b7a7ca27f54
Shappy <HapShapIncorporated@gmail.com>
2019-02-21 15:10:29 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var io = require('socket.io')(process.env.PORT||5000);
+var shortid = require('shortid');
+
+var players = [];
+var playerCount = 0;
+
+console.log("Server Running");
+
+io.on('connection', function(socket){
+    console.log("Connected to Unity");
+    var thisPlayerId = shortid.generate();
+
+    var player = {
+        id:thisPlayerId,
+        position:{
+            v:0
+        }
+    };
+
+    players[thisPlayerId] = player;
+
+    socket.broadcast.emit('spawn', {id:thisPlayerId});
+    console.log("Sending Spawn to New With ID", thisPlayerId);
+
+    for(var i = 0; i < players.length; i++){
+        socket.emit('spawn');
+
+    }
+
+    socket.on('sayhello', function(data){
+        console.log("Unity Game Says Hello");
+        socket.emit('talkback');
+    });
+
+    socket.on('disconnect', function(){
+        console.log("Player Disconnected");
+        playerCount--;
+    });
+
+    socket.on('move', function(data){
+        data.id = thisPlayerId;
+        console.log("Player Moved", JSON.stringify(data));
+        socket.broadcast.emit('move', data);
+    });
+});
